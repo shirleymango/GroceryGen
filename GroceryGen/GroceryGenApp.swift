9fca8bbb71bc8d49d2185a2c4e67b2b9884d2219
Shirley <shirleyzhu@college.harvard.edu>
2025-07-22 11:00:30 -0500

    Initial Commit

---
+++ GroceryGen/GroceryGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryGenApp.swift
+//  GroceryGen
+//
+//  Created by ZhuMacPro on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
